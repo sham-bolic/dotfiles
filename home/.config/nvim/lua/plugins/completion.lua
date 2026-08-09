b3f7291fc2f27be2b887ffeebc98a537ae7c2286
Max Fong <maxcyfong@gmail.com>
2026-08-09 01:56:29 -0400

    fix(nvim): stop accepting completion with right arrow

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -6,7 +6,6 @@ return {
       keymap = {
         preset = 'super-tab',
         ['<CR>'] = { 'select_and_accept', 'fallback' },
-        ['<Right>'] = { 'select_and_accept', 'fallback' },
       },
       appearance = { nerd_font_variant = 'mono' },
       completion = { documentation = { auto_show = true } },
